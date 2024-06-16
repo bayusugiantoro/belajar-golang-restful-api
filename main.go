5003f2c910ab737a035478ea0102f5c042f8327e
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-06-16 22:30:52 +0700

    http server

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"belajar-golang-restful-api/app"
 	"belajar-golang-restful-api/controller"
+	"belajar-golang-restful-api/helper"
 	"belajar-golang-restful-api/repository"
 	"belajar-golang-restful-api/service"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/go-playground/validator"
 	"github.com/julienschmidt/httprouter"
@@ -25,5 +29,13 @@ func main() {
 	router.POST("/api/categories", categoryController.Create)
 	router.PUT("/api/categories/:categoryId", categoryController.Update)
 	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
 
 }
